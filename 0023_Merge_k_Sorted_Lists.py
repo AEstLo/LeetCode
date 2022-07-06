083b58df914772fb8171ab612f7eb88fec69219f
AEstLo <palestebanez1@hotmail.com>
2022-07-07 01:30:42 +0200

    [ADD] 23. Merge k Sorted Lists

---
+++ 0023_Merge_k_Sorted_Lists.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # Being N the number of nodes and k the number of lists
+        # Time: O(N log k)
+        # Space: O(1)
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else None
+
+    def merge2Lists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head = node = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                node.next = l1
+                l1 = l1.next
+            else:
+                node.next = l2
+                l2 = l2.next
+            node = node.next
+        if l1:
+            node.next = l1
+        else:
+            node.next = l2
+        return head.next
