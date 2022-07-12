f7e887c2548833557ec903dc63eb318d4b1710e4
AEstLo <palestebanez1@hotmail.com>
2022-07-13 01:30:52 +0200

    [IMP] 0473 -> memoization

--- 0473_Matchsticks_to_Square.py
+++ 0473_Matchsticks_to_Square.py
@@ -22,3 +22,39 @@ class Solution:
             return False
 
         return isSquare(0)
+
+
+"""
+Solution with memoization
+
+
+def makesquare(self, matchsticks: List[int]) -> bool:
+        N = len(matchsticks)
+        matchsticks.sort(reverse=True)
+        # Time: O(4^N)
+        # Space: O(N)
+        square_sides = [0, 0, 0, 0]
+        total = sum(matchsticks)
+        if total % 4 != 0:
+            return False
+        square_border_size = total // 4
+        
+        memo = {}
+        
+        def isSquare(index):
+            key = f'{square_sides[0]},{square_sides[1],square_sides[2],square_sides[3]}'
+            if key in memo:
+                return memo[key]
+            if index == N:
+                return square_sides[0] == square_sides[1] == square_sides[2] == square_border_size
+            for i in range(4):
+                if square_sides[i] + matchsticks[index] <= square_border_size:
+                    square_sides[i] += matchsticks[index]
+                    if isSquare(index + 1):
+                        return True
+                    square_sides[i] -= matchsticks[index]
+            memo[key] = False
+            return False
+        
+        return isSquare(0)
+"""
