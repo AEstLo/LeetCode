517c15fb6a89ea83efb080e6245435ff943f3750
AEstLo <palestebanez1@hotmail.com>
2022-07-24 03:59:43 +0200

    [IMP] 3 SUM

--- 0015_three_sum.py
+++ 0015_three_sum.py
@@ -1,36 +1,29 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) < 3:
-            return []
-
-        nums.sort()
-        n = len(nums)
+        # Time: O(N^2)
+        # Space: O(N)
         ret = []
-        i = 0
-        while i < n and nums[i] < 0:
-            num = nums[i]
-            target = 0 - num
+        N = len(nums)
+        nums.sort()
 
-            l = i + 1
-            r = n - 1
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
+            l, r = i + 1, N - 1
+            target = -nums[i]
             while l < r:
-                sum_nums = nums[l] + nums[r]
-                if sum_nums == target:
-                    num2 = nums[l]
-                    num3 = nums[r]
-                    ret.append([num, nums[l], nums[r]])
-                    while l < r and nums[l] == num2:
-                        l += 1
-                    while l < r and nums[r] == num3:
-                        r -= 1
-                elif sum_nums > target:
-                    r -= 1
-                else:
+                val = nums[l] + nums[r]
+                if val == target:
+                    ret.append([a, nums[l], nums[r]])
                     l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+                elif val < target:
+                    l += 1
+                else:
+                    r -= 1
 
-            while i < n and num == nums[i]:
-                i += 1
-        if i < n - 2 and nums[i] == 0:
-            if nums[i] == 0 and nums[i + 1] == 0 and nums[i + 2] == 0:
-                ret.append([0, 0, 0])
         return ret
+
+# Runtime: 716 ms, faster than 94.56% of Python3 online submissions for 3Sum.
+# Memory Usage: 18.1 MB, less than 41.74% of Python3 online submissions for 3Sum.
