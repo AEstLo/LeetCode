7f7978c88065ac2152e6fab3817c3dba941cdab8
AEstLo <palestebanez1@hotmail.com>
2022-07-25 04:39:51 +0200

    [ADD] 595. Big Countries

---
+++ 595_Big_Countries.sql
@@ -0,0 +1,5 @@
+-- Write your MySQL query statement below
+SELECT name, population, area FROM World WHERE area >= 3000000 OR population >= 25000000;
+
+-- Runtime: 425 ms, faster than 21.77% of MySQL online submissions for Big Countries.
+-- Memory Usage: 0B, less than 100.00% of MySQL online submissions for Big Countries.
