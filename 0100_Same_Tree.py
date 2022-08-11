7021e8b069cf47a0eb5197466910a1bbd2e4aaaf
AEstLo <palestebanez1@hotmail.com>
2022-08-11 03:39:45 +0200

    IMP

--- 0100_Same_Tree.py
+++ 0100_Same_Tree.py
@@ -22,3 +22,31 @@ class Solution:
 
 # Runtime: 44 ms, faster than 59.23% of Python3 online submissions for Same Tree.
 # Memory Usage: 14 MB, less than 29.45% of Python3 online submissions for Same Tree.
+
+    def isSameTree_11_08_2022(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and q:
+            return False
+        if p and not q:
+            return False
+        if not q and not p:
+            return True
+        if p.val != q.val:
+            return False
+        if p.left and not q.left:
+            return False
+        if not p.left and q.left:
+            return False
+        if p.right and not q.right:
+            return False
+        if not p.right and q.right:
+            return False
+        if p.left:
+            if not self.isSameTree(p.left, q.left):
+                return False
+        if p.right:
+            if not self.isSameTree(p.right, q.right):
+                return False
+        return True
+
+# Runtime: 36 ms, faster than 83.33% of Python3 online submissions for Same Tree.
+# Memory Usage: 13.9 MB, less than 29.67% of Python3 online submissions for Same Tree.
