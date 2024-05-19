18476b17708d30b1b10ca81a7e02405cb3aa29c9
AEstLo <palestebanez1@hotmail.com>
2024-05-19 11:45:23 +0200

    [ADD] 0016_3Sum_Closest.py

--- 0016_3Sum_Closest.py
+++ 0016_3Sum_Closest.py
@@ -1,17 +1,17 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        min_heap = []
+        diff = (2 * 10**4) + 1  # taken from description
         for i, num in enumerate(nums):
             l, r = i + 1, len(nums) - 1
             while l < r:
                 total = num + nums[l] + nums[r]
                 if total == target:
                     return target
-                heapq.heappush(min_heap, (abs(total - target), total - target))
+                if abs(total - target) < abs(diff):
+                    diff = total - target
                 if total < target:
                     l += 1
                 else:
                     r -= 1
-        _, diff = heapq.heappop(min_heap)
         return diff + target
