0e55cc2087e4b65b3f54ffac41f69f8942f9b1f9
Álvaro Estébanez <87199854+AEstLo@users.noreply.github.com>
2024-05-29 17:08:42 +0200

    Update 1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.py

--- 1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.py
+++ 1404_Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One.py
@@ -1,5 +1,25 @@
 class Solution:
     def numSteps(self, s: str) -> int:
+        # In-place:
+        # Runtime 33ms Beats 77.59% of users with Python3
+        # Memory 16.42MB Beats 94.14% of users with Python3
+        steps = 0
+        carry = 0
+        for i in range(len(s) - 1, 0, -1):
+            if s[i] == "1":
+                if not carry:
+                    steps += 1
+                carry = 1
+            elif carry:
+                steps += 1
+            steps += 1
+
+        if carry:
+            steps += 1
+
+        return steps
+
+        # Converting to num
         number = 0
         for digit in s:
             number <<= 1
