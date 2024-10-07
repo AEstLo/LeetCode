788fab61c6be42042589b9d6499fcd5289ef0546
Álvaro Estébanez <87199854+AEstLo@users.noreply.github.com>
2024-10-07 16:08:41 +0200

    Create 860_Lemonade_Change.py

---
+++ 860_Lemonade_Change.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        FIVE = 0
+        TEN = 1
+        TWENTY = 2
+        money = [0, 0, 0]
+        for bill in bills:
+            if bill == 5:
+                money[FIVE] += 1
+            elif bill == 10:
+                money[TEN] += 1
+                money[FIVE] -= 1
+                if money[FIVE] < 0:
+                    return False
+            else:  # bill == 20
+                money[TWENTY] += 1
+                money[FIVE] -= 1
+                if money[TEN] > 0:
+                    money[TEN] -= 1
+                else:
+                    money[FIVE] -= 2
+                if money[TEN] < 0 or money[FIVE] < 0:
+                    return False
+        return True
