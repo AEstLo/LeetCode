3b65765f887034811793e96892fb368451e4aabb
Álvaro Estébanez <87199854+AEstLo@users.noreply.github.com>
2024-10-21 13:05:04 +0200

    Update 0042_Trapping_Rain_Water.py

--- 0042_Trapping_Rain_Water.py
+++ 0042_Trapping_Rain_Water.py
@@ -2,26 +2,38 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         # Being N the number of elements
         # Time: O(N)
-        # Space: O(N)
-        N = len(height)
-        max_from_left = [0] * N
-        max_from_right = [0] * N
+        # Space: O(1)
+        l = 0
+        n = len(height)
+        while l < n - 1 and height[l] < height[l + 1]:
+            l += 1
+        r = l + 1
+        result = 0
+        while r < n:
+            while r < n and height[l] > height[r]:
+                r += 1
+            if r < n:
+                min_height = min(height[l], height[r])
+                l += 1
+                while l < r:
+                    result += min_height - height[l]
+                    l += 1
+                r += 1
 
-        current_max = 0
-        for i, h in enumerate(height):
-            max_from_left[i] = current_max
-            if h > current_max:
-                current_max = h
+        peak = l
+        r = n - 1
+        while r > 0 and height[r] < height[r - 1]:
+            r -= 1
+        l = r - 1
+        while l >= peak:
+            while l >= 0 and height[r] > height[l]:
+                l -= 1
+            if l >= 0:
+                min_height = min(height[l], height[r])
+                r -= 1
+                while l < r:
+                    result += min_height - height[r]
+                    r -= 1
+                l -= 1
 
-        current_max = 0
-        for i in range(len(height) - 1, -1, -1):
-            h = height[i]
-            max_from_right[i] = current_max
-            if h > current_max:
-                current_max = h
-
-        total = 0
-        for i, h in enumerate(height):
-            if max_from_left[i] > h and max_from_right[i] > h:
-                total += min(max_from_left[i], max_from_right[i]) - h
-        return total
+        return result
